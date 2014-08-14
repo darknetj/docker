ff817860f6f2d8bb814c1fafbb6f94cfc175e053
Bruno Oliveira <bruno@abstractj.org>
2014-08-14 07:03:05 -0300

    Inclusion of wildfly image

---
+++ aerogear/wildfly/quickstarts-dev/launch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##############################################################
+# Run Maven, deploy the war file and start the server
+##############################################################
+
+source $HOME/bin/quickstart-config.sh
+
+launch() {
+  if [ -d $HOME/$REPO_NAME ] && [ -f $CONFIG_FILE ];then
+    echo "We are about to download the whole internet, be patient" | cowsay
+    cd $HOME/$REPO_NAME && mvn -s quickstart-settings.xml clean install -DskipTests=true
+    cp $HOME/$REPO_NAME/server/contacts-mobile-picketlink-secured/target/jboss-contacts-mobile-picketlink-secured.war $JBOSS_HOME/standalone/deployments/
+  fi
+  $JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+}
+
+##############################################################
+# Check if the argument was informed during the boot time
+# otherwise just call bash
+##############################################################
+if [ ! -z "$CONFIG" ]; then
+  echo $CONFIG > $CONFIG_FILE
+  launch; /bin/bash
+elif [ "$1" = "setup" ]; then
+  config
+else
+  /bin/bash
+fi
+
